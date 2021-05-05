d91b7ccd95b9c35cb5d706a1ff9b927ca96b9a8f
Jean-Baptiste Lavisse <jean-baptiste@jblavisse.fr>
2021-05-05 10:16:34 +0200

    initial commit

---
+++ select.sql
@@ -0,0 +1,12 @@
+-- Lister tous les tags (T: tag) avec leur nom (C: name)
+SELECT name 
+FROM tag;
+
+-- Lister tous les étudiants (T: student) 
+-- avec leur prénom (C: firstname), et le nom (C: lastname)
+SELECT firstname, lastname
+FROM student;
+
+-- Lister tous les projets avec toutes leurs caractéristiques
+SELECT * 
+FROM project;
